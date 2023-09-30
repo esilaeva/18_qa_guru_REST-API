db3a43600ba8b6925ea7f32299e15c471885af2f
esilaeva <emsilaeva@gmail.com>
2023-09-30 18:03:59 +0300

    first commit * homework 18 (REST-API)

---
+++ src/test/java/guru/qa/tests/InReqresTests.java
@@ -0,0 +1,122 @@
+package guru.qa.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class InReqresTests extends TestBase{
+
+    @Test
+    void successfulLoginTest() {
+        String authData = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void getListUsers() {
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .when()
+                .get("/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("page", is(2));
+    }
+
+    @Test
+    void createUser() {
+        String authData = "{\"name\": \"morpheus\", \"job\": \"leader\"}";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void updateUser() {
+        String authData = "{\"name\": \"morpheus\", \"job\": \"zion resident\"}";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void successfulRegisterUser() {
+        String authData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\"}";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("email", is("eve.holt@reqres.in"));
+    }
+
+    @Test
+    void unsuccessfulRegisterUser() {
+        String authData = "{\"email\": \"sydney@fife\"}";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+}
